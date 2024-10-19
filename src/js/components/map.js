1dede9bc1dc2d1ce5557f315e264abcd544c8d83
xopowen <xopowen@gmail.com>
2024-10-19 14:46:57 +0800

    v2.331

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,14 +1,50 @@
 //заружает скрыпты карты и init карту после загрузки страницы если на странице есть id = "map"
-if(document.querySelector('#map')){
-    setTimeout(async function(){
-        var elem = document.createElement('script');
-        elem.type = 'text/javascript';
-        elem.src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=aae3f3e4-374f-420f-a948-677e1a58f577";
-        elem.type="text/javascript";
-        elem.onload = ev => mapInit()
-        document.body.appendChild(elem);
-    }, 2000);
+
+
+async function loadMapScript(){
+    var elem = document.createElement('script');
+    elem.type = 'text/javascript';
+    elem.src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=aae3f3e4-374f-420f-a948-677e1a58f577";
+    elem.type="text/javascript";
+    elem.onload = ev => mapInit()
+    document.body.appendChild(elem);
 }
+
+if(document.querySelector('#map')){
+    if(window.IntersectionObserver){
+        var options = {
+            threshold:  0.1,
+        };
+        /**
+         *
+         * @param {  Array<IntersectionObserverEntry>} entries
+         * @param observer
+         */
+        let callBack = function (entries, observer){
+            console.log('callBack is called')
+            entries.forEach(entrie=>{
+                if(entrie.isIntersecting){
+                    if(!entrie.target.dataset.scriptStart){
+                        entrie.target.dataset.scriptStart = true+''
+                        loadMapScript()
+                    }
+
+                }
+            })
+        }
+
+        let obser = new IntersectionObserver(callBack,options )
+        obser.observe(document.querySelector('#map'))
+
+    }else{
+
+    }
+
+    setTimeout( loadMapScript, 2000);
+}
+
+
+
 
 async function mapInit(){
 
